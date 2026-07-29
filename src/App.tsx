ae3e84396211d5729d3dd9458a16b86e193d5492
Heet Vachhani <vachhaniheet1504@gmail.com>
2026-07-29 12:35:20 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>FlowSensei</h1>
+      <p>Welcome to your Kanban board with AI retrospectives.</p>
+    </div>
+  );
+};
+
+export default App;
